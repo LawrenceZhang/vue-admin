0275d63b1a1f8a5c5692a0105e0ed45a5addf5c6
z00350820 <lawrence.zhang@huawei.com>
2017-12-20 18:41:34 +0800

    dev

--- src/api/products.js
+++ src/api/products.js
@@ -1,9 +1,41 @@
 import fetch from '@/utils/fetch'
 
-export function getProducts(params) {
+export function getProductList(params) {
   return fetch({
     url: '/queryproduct',
     method: 'get',
     params
+  })
+}
+
+export function addProduct(data) {
+  return fetch({
+    url: '/addproduct',
+    method: 'post',
+    data
+  })
+}
+
+export function updateProduct(data) {
+  return fetch({
+    url: '/modifyproduct',
+    method: 'put',
+    data
+  })
+}
+
+export function deleteProduct(data) {
+  return fetch({
+    url: '/deleteproduct',
+    method: 'delete',
+    data
+  })
+}
+
+export function modifyInventory(data) {
+  return fetch({
+    url: '/modifyproductinventory',
+    method: 'delete',
+    data
   })
 }
